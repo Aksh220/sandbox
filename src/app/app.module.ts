95817d9650b688567f251475f94cdcdc76ca8a5b
Aksh2209 <akshayap672@gmail.com>
2020-05-21 18:20:35 +0530

    2 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AngularMaterial } from './angular.material';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
@@ -8,8 +12,12 @@ import { FooterComponent } from './global/footer/footer.component';
 import { HeaderComponent } from './global/header/header.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, FooterComponent, HeaderComponent ],
+  imports: [
+    BrowserModule, FormsModule,ReactiveFormsModule, AngularMaterial,BrowserAnimationsModule, HttpClientModule
+  ],
+  declarations: [
+    AppComponent, HelloComponent, FooterComponent, HeaderComponent
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
