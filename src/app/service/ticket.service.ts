0e9f00c0a88632371ef720ea28acfb9cd83b26a8
Aksh2209 <akshayap672@gmail.com>
2020-05-22 12:14:41 +0530

    4 files modified

--- src/app/service/ticket.service.ts
+++ src/app/service/ticket.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
 
-@Injectable()
+export interface Ticket {
+  ticketId: string,
+  ticketOwner: string,
+  ticketSubject: string,
+  ticketMessage:string
+}
+
+@Injectable({
+  providedIn: 'root'
+})
 export class TicketService {
+  constructor(private http : HttpClient) { }
+  url : string = "https://5eb39595974fee0016ecd67c.mockapi.io/demo/tickets"
 
+  getTicket()
+  {
+    return this.http.get<Ticket[]>(this.url);
+  }
 }
--- src/app/ticket/ticket.component.ts
+++ src/app/ticket/ticket.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TicketService } from '../service/ticket.service';
+import { Ticket } from '../service/ticket.service';
 
 @Component({
   selector: 'ticket-component',
   templateUrl: 'ticket.component.html'
 })
-export class TicketComponent  {
-  
+export class TicketComponent implements OnInit {
+  constructor(private rs : TicketService) { }
+
+  columns = ["Ticket Id","Ticket Owner","Ticket Subject", "Ticket Message"];
+  index = ["support_ticket_id", "userId", "ticket_subject", "ticket_description"];
+
+  tickets : Ticket[] = [];
+
+  ngOnInit(): void {
+     this.rs.getTicket().subscribe
+     (
+       (response)=>
+       {
+         this.tickets = response;
+       },
+       (error) => console.log(error)
+     )
+   }
 }
